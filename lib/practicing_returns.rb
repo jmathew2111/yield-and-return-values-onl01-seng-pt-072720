fa41ee0d67410413a822b57c4f461a2d46ce12e1
Justin Mathew <jmathew2111@yahoo.com>
2020-07-31 18:51:13 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,1 +1,10 @@
     hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+    def hello(array)
+      i = 0
+      collection = []
+      while i < array.length
+        collection << yield(array[i])
+        i += 1
+      end
+      collection
+    end
